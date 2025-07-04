dfd04a71ee51c7636424579622c14551d40f31db
ArnaudM1983 <arnaudmeusy@hotmail.com>
2025-07-04 15:21:37 +0200

    create job03

---
+++ jour01/job03/index.php
@@ -0,0 +1,72 @@
+<?php
+
+$boolVar = true;
+$intVar = 69;
+$stringVar = "LaPlateforme";
+$floatVar = 6.14;
+var_dump($floatVar);
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Types de Variables PHP</title>
+    <style>
+        table {
+            border-collapse: collapse;
+            width: 50%;
+        }
+
+        th,
+        td {
+            border: 1px solid #333;
+            padding: 8px;
+        }
+
+        th {
+            background-color: #eee;
+        }
+    </style>
+</head>
+
+<body>
+
+    <h2>Tableau des variables PHP</h2>
+
+    <table>
+        <thead>
+            <tr>
+                <th>Type</th>
+                <th>Nom</th>
+                <th>Valeur</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td><?php echo gettype($boolVar); ?></td>
+                <td>boolVar</td>
+                <td><?php echo $boolVar; ?></td>
+            </tr>
+            <tr>
+                <td><?php echo gettype($intVar); ?></td>
+                <td>intVar</td>
+                <td><?php echo $intVar; ?></td>
+            </tr>
+            <tr>
+                <td><?php echo gettype($stringVar); ?></td>
+                <td>stringVar</td>
+                <td><?php echo $stringVar; ?></td>
+            </tr>
+            <tr>
+                <td><?php echo gettype($floatVar); ?></td>
+                <td>floatVar</td>
+                <td><?php echo $floatVar; ?></td>
+            </tr>
+        </tbody>
+    </table>
+
+</body>
+
+</html>
