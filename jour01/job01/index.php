8d67d13edf78671e5709706dcef14d33de31d367
ArnaudM1983 <arnaudmeusy@hotmail.com>
2025-07-04 15:01:41 +0200

    create all variables in job01

--- jour01/job01/index.php
+++ jour01/job01/index.php
@@ -1,4 +1,25 @@
 <?php
+
+// ----- Variables de chaînes de caractères -----
 $str = "LaPlateforme";
-echo $str;
+echo $str . "<br>";
+
+$str2 = "Vive";
+$str3 = "!";
+echo $str2 . " " . $str . " " . $str3 . "<br>";
+
+// ----- Variable numérique -----
+$val = 6;
+echo $val . "<br>";
+
+$val += 4;
+echo $val . "<br>";
+
+// ----- Variable booléenne -----
+$myBool = true;
+echo $myBool . "<br>";  
+
+$myBool = false;
+echo $myBool . "<br>"; 
+
 ?>
