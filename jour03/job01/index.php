9a0caa1075d53fb3b4e39111da0e4adfe37b1abc
ArnaudM1983 <arnaudmeusy@hotmail.com>
2025-07-10 14:44:10 +0200

    update jour03/job01

--- jour03/job01/index.php
+++ jour03/job01/index.php
@@ -1,5 +1,15 @@
 <?php
 
-$list = [200, 204, 173, 98, 171, 404, 459]
+$numbers = [200, 204, 173, 98, 171, 404, 459];
+
+foreach ($numbers as $number) {
+
+    if ($number %2 == 0) {
+        echo "$number est pair <br />";
+    } else {
+        echo "$number est impair <br />";
+    }
+    
+}
 
 ?>
