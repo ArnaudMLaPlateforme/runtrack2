d8a15cb9e760323b047e0462612fc092fb05b469
ArnaudM1983 <arnaudmeusy@hotmail.com>
2025-07-10 14:04:55 +0200

    update job07

--- jour02/job07/index.php
+++ jour02/job07/index.php
@@ -1,5 +1,30 @@
 <?php
 
+$hauteur = 5;
 
+for ($i = 0; $i < $hauteur; $i++) {
+
+    // Espaces à gauche (pour centrer le triangle)
+    for ($espace = 0; $espace < $hauteur - $i - 1; $espace++) {
+        echo "&nbsp;&nbsp;";
+    }
+
+    // Début de ligne
+    echo "/";
+
+    // Espace du milieu OU underscore sur la dernière ligne
+    for ($milieu = 0; $milieu < $i * 2; $milieu++) {
+        // Si dernière ligne
+        if ($i == $hauteur - 1) {
+            echo "_";
+        } else {
+            echo "&nbsp;&nbsp;";
+        }
+    }
+
+    echo "\\";
+
+    echo "<br />";
+}
 
 ?>
