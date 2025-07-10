774a466af8ef326bf1c88a8cce080827baae5cbf
ArnaudM1983 <arnaudmeusy@hotmail.com>
2025-07-10 10:57:05 +0200

    create job05

---
+++ jour02/job05/index.php
@@ -0,0 +1,22 @@
+<?php
+// On commence à 2 car 1 n'est pas un nombre premier
+for ($nombre = 2; $nombre <= 1000; $nombre++) {
+
+    $estPremier = true;
+
+    // On vérifie s'il est divisible par un autre nombre (sauf lui-même et 1)
+    for ($diviseur = 2; $diviseur < $nombre; $diviseur++) {
+        
+        if ($nombre % $diviseur == 0) {
+            $estPremier = false;
+            break; // on sort de la boucle car on a trouvé un diviseur
+        }
+    }
+
+    // Si on n'a trouvé aucun diviseur, alors c'est un nombre premier
+    if ($estPremier) {
+        echo $nombre . "<br />";
+    }
+}
+
+?>
