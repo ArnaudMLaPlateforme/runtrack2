83a50693db508ab7b4fb40c274bff1608535ce07
ArnaudM1983 <arnaudmeusy@hotmail.com>
2025-07-15 11:18:32 +0200

    update jour03/job04

--- jour03/job04/index.php
+++ jour03/job04/index.php
@@ -6,7 +6,12 @@ dans cette chaîne. -->
 
 <?php
 
+$str = "Dans l'espace, personne ne vous entend crier.";
+$count = 0;
 
-
+for ($i=0; isset($str[$i]) ; $i++) { 
+    $count++;
+}
+echo "Nombre de caractères : $count";
 
 ?>
