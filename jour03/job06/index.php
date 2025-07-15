55965725cab246b337d36f99e417428f9d1f77cf
ArnaudM1983 <arnaudmeusy@hotmail.com>
2025-07-15 16:48:10 +0200

    update jour03/job06

--- jour03/job06/index.php
+++ jour03/job06/index.php
@@ -3,8 +3,26 @@
 Créez un algorithme qui parcourt et écrit cette chaine à l’envers.
 Ex. : redessop suon rap tnessinif edessop no'l euq sesohc seL -->
 
+
 <?php
 
+$str = "Les choses que l'on possede finissent par nous posseder.";
 
+// On initialise une variable $longueur à 0
+$longueur = 0;
+
+// Calcul de la longueur de $str
+// On utilise une boucle while pour parcourir chaque caractère de $str
+// On vérifie si un caractère existe à l'indice $longueur avec isset($str[$longueur])
+// Tant que c’est vrai, on augmente $longueur de 1
+while (isset($str[$longueur])) {
+    $longueur++;
+}
+
+// On commence à la fin de $str ($longueur - 1) et on va jusqu'au début de $str
+for ($i = $longueur - 1; $i >= 0; $i--) {
+
+    echo $str[$i];
+}
 
 ?>
