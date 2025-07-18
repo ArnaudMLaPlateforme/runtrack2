d49763221bcaf13952de3fc75d8586bd83c1bd4b
ArnaudM1983 <arnaudmeusy@hotmail.com>
2025-07-18 16:29:31 +0200

    create jour04/job03

---
+++ jour04/job03/index.php
@@ -0,0 +1,6 @@
+<!-- Développez un algorithme qui affiche le nombre d’arguments $_POST.
+Tips :
+Pour tester votre code, créez un formulaire html <form> de type POST avec différents
+champs <input> de type “text” et un <input> de type “submit” pour l’envoi.
+Vous pouvez ensuite afficher avec echo directement dans votre page par exemple :
+“Le nombre d’argument POST envoyé est : “ -->
