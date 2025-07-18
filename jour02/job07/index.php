d790d6dbf35f3b5eb484254d80b3d5a65f92d695
ArnaudM1983 <arnaudmeusy@hotmail.com>
2025-07-18 14:59:41 +0200

    update jour02/job07

--- jour02/job07/index.php
+++ jour02/job07/index.php
@@ -1,30 +1,63 @@
 <?php
 
-$hauteur = 10;
+$hauteur = 5;
 
 for ($i = 0; $i < $hauteur; $i++) {
 
-    // Espaces à gauche (pour centrer le triangle)
-    for ($espace = 0; $espace < $hauteur - $i - 1; $espace++) {
-        echo "&nbsp;&nbsp;";
+    // Calcule du nombre d'espaces à gauche pour centrage
+    // Chaque "bloc" central fait 3 caractères visuels (1 underscore + 1 espace = 3 avec HTML spacing)
+    $espacesGauche = ($hauteur - $i - 1) * 3;
+
+    // Affiche les espaces pour centrer
+    for ($espace = 0; $espace < $espacesGauche; $espace++) {
+        echo "&nbsp;";
     }
 
-    // Début de ligne
+    // Bord gauche du triangle
     echo "/";
 
-    // Espace du milieu OU underscore sur la dernière ligne
+    // Centre : espaces ou "_&nbsp;" selon la ligne
     for ($milieu = 0; $milieu < $i * 2; $milieu++) {
-        // Si dernière ligne
         if ($i == $hauteur - 1) {
-            echo "_";
+            echo "_&nbsp;";
         } else {
-            echo "&nbsp;&nbsp;";
+            echo "&nbsp;&nbsp;&nbsp;"; 
         }
     }
 
+    // Bord droit du triangle
     echo "\\";
 
+    // Retour à la ligne
     echo "<br />";
 }
+
+
+// $hauteur = 10;
+
+// for ($i = 0; $i < $hauteur; $i++) {
+
+//     // Espaces à gauche (pour centrer le triangle)
+//     for ($espace = 0; $espace < $hauteur - $i - 1; $espace++) {
+//         echo "&nbsp;&nbsp;";
+//     }
+
+//     // Début de ligne
+//     echo "/";
+
+//     // Espace du milieu OU underscore sur la dernière ligne
+//     for ($milieu = 0; $milieu < $i * 2; $milieu++) {
+//         // Si dernière ligne
+//         if ($i == $hauteur - 1) {
+//             echo "_";
+//         } else {
+//             echo "&nbsp;&nbsp;";
+//         }
+//     }
+
+//     echo "\\";
+
+//     echo "<br />";
+// }
 
 ?>
