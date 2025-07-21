ef87d16b4de4624dcb9068e240df0bf35c0f6ba9
ArnaudM1983 <arnaudmeusy@hotmail.com>
2025-07-21 16:27:01 +0200

    update jour04/job07

--- jour04/job07/index.php
+++ jour04/job07/index.php
@@ -7,16 +7,68 @@ maison qui s’affiche sur la page doit ressembler à ceci : -->
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <form action="" method="POST">
         Largeur: <input type="text" name="largeur">
         Hauteur: <input type="text" name="hauteur">
         <input type="submit">
     </form>
+
+    <?php
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $largeur = $_POST["largeur"];
+        $hauteur = $_POST["hauteur"];
+
+        for ($i = 0; $i < $hauteur; $i++) {
+
+            // Espaces à gauche (pour centrer le triangle)
+            for ($espace = 0; $espace < $hauteur - $i - 1; $espace++) {
+                echo "&nbsp;&nbsp;";
+            }
+
+            // Début de ligne
+            echo "/";
+
+            // Espace du milieu OU underscore sur la dernière ligne
+            for ($milieu = 0; $milieu < $i * 2; $milieu++) {
+                echo "_";
+            }
+
+            echo "\\";
+
+            echo "<br />";
+        }
+    }
+
+    for ($i = 0; $i < $hauteur; $i++) {
+    for ($j = 0; $j < $largeur; $j++) {
+        // Bord gauche et bord droit
+        if ($j == 0 || $j == $largeur - 1) {
+            echo "|";
+        }
+        // Si on est sur la dernière ligne
+        elseif ($i == $hauteur - 1) {
+            echo "_";
+        }
+        // Espaces à l'intérieur
+        else {
+            echo "&nbsp;&nbsp;";
+        }
+    }
+    echo "<br />";
+}
+
+
+    ?>
+
 </body>
+
 </html>
