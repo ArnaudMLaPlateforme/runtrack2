32da176780fa289dd5418dd45462f4c8ceb205cb
ArnaudM1983 <arnaudmeusy@hotmail.com>
2025-07-22 15:07:19 +0200

    update jour07/job02

---
+++ jour07/job02/index.php
@@ -0,0 +1,21 @@
+<!-- Créez une fonction nommée “bonjour($jour)”.
+Cette fonction prend en paramètre un booléen nommé “$jour”.
+● Si le paramètre “$jour” vaut true, la fonction doit afficher : “Bonjour”,
+● Si le paramètre “$jour” vaut false, la fonction doit afficher : “Bonsoir”. -->
+
+<?php
+
+function bonjour($jour){
+    if ($jour == true) {
+        echo "Bonjour";
+    } else {
+        echo "Bonsoir";
+    }
+}
+
+// Appel de la fonction
+bonjour(true);
+echo "<br />";
+bonjour(false);
+
+?>
