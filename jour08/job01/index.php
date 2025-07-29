b268b5ef58215e559caad6a8020e6f2a4a267f58
ArnaudM1983 <arnaudmeusy@hotmail.com>
2025-07-29 13:20:22 +0200

    update jour08/job01

---
+++ jour08/job01/index.php
@@ -0,0 +1,55 @@
+<!-- Créez une variable de session nommée “nbvisites”. A chaque fois que la page est
+visitée, ajoutez 1. Afficher le contenu de cette variable.
+Ajoutez un bouton nommé “reset” qui permet de réinitialiser ce compteur.
+
+Début
+
+    Démarrer la session
+
+    Si la variable de session "nbvisites" n'existe pas ALORS
+        L'initialiser à 0
+    Fin Si
+
+    Si le bouton "reset" a été cliqué (c’est-à-dire si la clé "reset" existe dans $_POST) ALORS
+        Remettre la variable de session "nbvisites" à 0
+    SINON
+        Incrémenter la variable de session "nbvisites" de 1
+    Fin Si
+
+    Afficher le nombre de visites dans le HTML
+
+    Afficher un formulaire HTML :
+        - avec un bouton nommé "reset"
+        - méthode POST
+
+Fin -->
+
+<?php
+session_start();
+
+if (!isset($_SESSION["nbvisites"])) {
+    $_SESSION["$nbvisites"] = 0;
+}
+
+if (isset($_POST["reset"])) {
+    $_SESSION["nbvisites"] = 0;
+} else {
+    $_SESSION["nbvisites"] = $_SESSION["nbvisites"] + 1;
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>Nombre de visites : <?php echo $_SESSION["nbvisites"] ?></p>
+    <form action="" method="POST">
+        <input type="submit" name="reset" value="Réinitialiser">
+    </form>
+</body>
+</html>
