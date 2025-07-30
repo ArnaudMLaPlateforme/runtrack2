32dbeb40c1b8a9851f14ffbf733bd72531988b51
ArnaudM1983 <arnaudmeusy@hotmail.com>
2025-07-30 13:20:08 +0200

    create jour09/job09

--- jour09/job09/job09.sql
+++ jour09/job09/job09.sql
@@ -1,6 +1,6 @@
-SELECT * 
-FROM etudiants 
+SELECT *
+FROM etudiants
 -- CURDATE() : donne la date du jour.
 -- DATE_SUB(CURDATE(), INTERVAL 18 YEAR) : calculer la date d’il y a 18 ans
 -- naissance <= ... : sélectionne les étudiants nés il y a 18 ans ou plus, donc ayant plus de 18 ans.
-WHERE naissance <= DATE_SUB(CURDATE(), INTERVAL 18 YEAR);
+WHERE naissance > DATE_SUB(CURDATE(), INTERVAL 18 YEAR);
