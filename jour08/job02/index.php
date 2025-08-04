3c1825c93b92f00c7e240a85ffcd1f679a5a1a93
ArnaudM1983 <arnaudmeusy@hotmail.com>
2025-08-04 10:45:26 +0200

    update jour08/job02

--- jour08/job02/index.php
+++ jour08/job02/index.php
@@ -4,60 +4,51 @@ Ajoutez un bouton nommé “reset” qui permet de réinitialiser ce compteur.
  
 Début
 
-    Donner un nom au cookie ("nbvisites")
-
-    Si l'utilisateur a cliqué sur le bouton "reset" ALORS
-        - Remettre la valeur du cookie à 0
-        - Stocker 0 dans une variable "visites" (pour l'affichage)
-    SINON
-        - Si le cookie existe déjà ALORS
-            - Récupérer sa valeur en lui ajoutant 1 et stocker le nouveau total dans la 
-            variable "visites"
-        SINON
-            - C'est la première visite
-            - Mettre la variable "visites" à 1
-
-        - Mettre à jour le cookie avec la nouvelle valeur de "visites"
-
-    FIN SI
-
-    Afficher le texte : "Nombre de visites : [valeur de visites]"
-
-    Afficher un formulaire avec un bouton "Reset"
-    (le formulaire utilise la méthode POST)
+Si le bouton "reset" a été cliqué alors
+    Supprimer le cookie "nbvisites" (en le faisant expirer)
+    Initialiser le compteur de visites à 0
+Sinon
+    Si le cookie "nbvisites" existe alors
+        Récupérer la valeur du cookie dans "visites"
+        Ajouter 1 à "visites"
+    Sinon
+        Initialiser "visites" à 1 (première visite)
+    FinSi
+    Mettre à jour le cookie "nbvisites" avec la valeur "visites"
+    Définir une durée de vie du cookie à 1 heure
+FinSi
 
 Fin
+
 
 -->
 
 <?php
 
-$cookie_name = "nbvisites";
-
-// Si l'utilisateur a cliqué sur le bouton "reset"
+// Si le bouton "reset" a été cliqué (formulaire soumis)
 if (isset($_POST["reset"])) {
-
-    // Remettre la valeur du cookie à 0
-    setcookie($cookie_name, 0);
+    // Supprimer le cookie en le faisant expirer dans le passé
+    setcookie("nbvisites", "", time() - 3600);
+    // Réinitialiser le compteur à 0
     $visites = 0;
-
 } else {
-
-    // Si le cookie existe déjà
-    if (isset($_COOKIE[$cookie_name])) {
-
-        // Récupérer sa valeur en lui ajoutant 1 et stocker le nouveau total dans la variable "visites"
-        $visites = $_COOKIE[$cookie_name] + 1;
+    // Sinon, si le cookie existe déjà
+    if (isset($_COOKIE["nbvisites"])) {
+        // Récupérer la valeur du cookie
+        $visites = $_COOKIE["nbvisites"];
+        // Incrémenter le compteur de visites
+        $visites++;
     } else {
-        // C'est la première visite
+        // Si le cookie n'existe pas, c'est la première visite
         $visites = 1;
     }
-
-    // Mettre à jour le cookie avec la nouvelle valeur de "visites"
-    setcookie($cookie_name, $visites);
+    // Mettre à jour le cookie avec la nouvelle valeur
+    // Le cookie durera 1 heure
+    setcookie("nbvisites", $visites, time() + 3600);
 }
 
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
